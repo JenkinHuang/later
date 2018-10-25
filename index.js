3c95c6b7c05be025b745de57b5ec7de29d259b81
Hikari <hikarizc@gmail.com>
2018-10-25 21:53:43 +0800

    add body-parser

--- index.js
+++ index.js
@@ -14,8 +14,12 @@
 const express = require("express");
 const app = express();
 const articles = [{ title: 'Example'}];
+const bodyParser = require("body-parser");
 
 app.set("port", process.env.PORT || 3000);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( { extended: true }));
 
 app.get("/articles", (req, res, next) => {
     res.send(articles);
